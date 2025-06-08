199b17aba79c318a0514847590539f960bd32a5a
Dinesh Kumar <48156150+acratophorux@users.noreply.github.com>
2025-06-08 13:32:59 +0000

    implemented identity recon logic

--- src/controllers/identify.controller.ts
+++ src/controllers/identify.controller.ts
@@ -1,4 +1,11 @@
 import {Request, Response} from 'express';
+import { identifyContact } from '../services/identify.service';
 export const identifyController = async (req: Request, res:Response)=>{
-    res.json({message:'identify endpoint here'});
+    try {
+        const result = await identifyContact(req.body);
+        res.status(200).json(result);
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    }
 };
---
+++ src/services/identify.service.ts
@@ -0,0 +1,91 @@
+import { AppDataSource } from "../data-source";
+import { Contact } from "../entities/contact.entity";
+import { In } from "typeorm";
+
+interface IdentifyRequest {
+    email?: string;
+    phoneNumber?: string;
+}
+
+export const identifyContact = async ({email, phoneNumber}: IdentifyRequest) =>{
+    const contactRepo = AppDataSource.getRepository(Contact);
+
+    // find all matching contacts
+    const existingContacts = await contactRepo.find({
+        where:[
+            {email}, {phoneNumber}
+        ],
+        relations: ['linkedContact', 'secondaryContacts'],
+        withDeleted: false,
+    });
+
+    //gather all unique contacts primary as well secondary
+    const allContacts = new Map<number, Contact>();
+    for(const contact of existingContacts){
+        if(contact.linkPrecedence ==='primary'){
+            allContacts.set(contact.id, contact);
+            contact.secondaryContacts?.forEach(sc => allContacts.set(sc.id, sc));
+        } else if (contact.linkedContact){
+            allContacts.set(contact.linkedContact.id, contact.linkedContact);
+            const siblings = await contactRepo.find({
+                where: {
+                    linkedContact: {id:contact.linkedContact.id}
+                },
+            });
+            siblings.forEach(sib=>allContacts.set(sib.id, sib));
+        }
+    }
+
+    // if no match found, then create primary contact
+    if(allContacts.size === 0){
+        const newContact = contactRepo.create({
+            email, phoneNumber, linkPrecedence:'primary'
+        });
+        await contactRepo.save(newContact);
+        return formatResponse(newContact, []);
+    }
+    //find the oldest primary contacct
+    const contactsArray = Array.from(allContacts.values());
+    const primary = contactsArray.filter( c => c.linkPrecedence === 'primary').sort((a, b)=> a.createdAt.getTime() - b.createdAt.getTime())[0];
+
+    //create new secondary when needed
+    const exists = contactsArray.some(c=> c.email === email && c.phoneNumber === phoneNumber);
+
+
+
+    let newSecondary: Contact | undefined = undefined;
+    if (!exists) {
+        newSecondary = contactRepo.create({
+            email, phoneNumber, linkPrecedence: 'secondary', linkedContact: primary
+        });
+        await contactRepo.save(newSecondary);
+    }
+
+    const allLinkedContacts = await contactRepo.find({
+        where: [{id:primary.id}, {linkedContact: {id: primary.id}}],
+        withDeleted: false
+    });
+    return formatResponse(primary, allLinkedContacts.filter(c=>c.id!==primary.id));
+}
+
+const formatResponse = (primary: Contact, secondaries: Contact[])=>{
+    const emails = new Set<string>();
+    const phoneNumbers = new Set<string>();
+    if(primary.email) emails.add(primary.email);
+    if(primary.phoneNumber) phoneNumbers.add(primary.phoneNumber);
+
+    secondaries.forEach(c=>{
+        if(c.email) emails.add(c.email);
+        if(c.phoneNumber) phoneNumbers.add(c.phoneNumber);
+    });
+
+    return {
+        contact: {
+            primaryContactId: primary.id,
+            emails: Array.from(emails),
+            phoneNumbers: Array.from(phoneNumbers),
+            secondaryContactIds: secondaries.map(c=>c.id)
+        }
+    }
+
+}
